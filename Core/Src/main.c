2626172e32650e89936902b1a6969b30583d6d2f
politeAtreus <anirudh.nair@dal.ca>
2025-11-22 01:41:33 -0400

    changed spi speed to 8Mhz

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -223,7 +223,6 @@ int main(void)
   MX_TIM2_Init();
   MX_TIM3_Init();
   MX_UART5_Init();
-
   /* USER CODE BEGIN 2 */
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
   HAL_TIM_Base_Start_IT(&htim3);      // start periodic update IRQ
